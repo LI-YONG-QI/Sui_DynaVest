4b243a70394ebcb5e01d761aa744052aad6ae3ae
LI-YONG-QI <a2369028@gmail.com>
2025-04-21 23:03:55 +0800

    feat: camelot stake xGrail implement

--- src/app/utils/strategies/camelot.ts
+++ src/app/utils/strategies/camelot.ts
@@ -3,11 +3,16 @@ import { arbitrum } from "viem/chains";
 
 import { BaseStrategy } from "./base";
 import { CAMELOT_CONTRACTS } from "../constants/protocols/camelot";
-import { waitForTransactionReceipt, writeContract } from "@wagmi/core";
+import {
+  waitForTransactionReceipt,
+  writeContract,
+  readContract,
+} from "@wagmi/core";
 import { wagmiConfig } from "@/providers/config";
 import { CAMELOT_STRATEGY_ABI } from "@/app/abis/camelotStrategy";
-import { GRAIL, WETH } from "../constants/coins";
-// import { xGRAIL } from "../constants/coins";
+import { GRAIL, WETH, xGRAIL } from "../constants/coins";
+import { ERC20_ABI } from "@/app/abis/erc20";
+import { XGRAIL_ABI } from "@/app/abis/xGrail";
 
 type CamelotSupportedChains = typeof arbitrum.id;
 
@@ -31,7 +36,7 @@ export class CamelotStrategy extends BaseStrategy<CamelotSupportedChains> {
   ): Promise<string> {
     if (asset == null) {
       const grail = GRAIL.chains![this.chainId];
-      //   const xGrail = xGRAIL.chains![this.chainId];
+      const xGrail = xGRAIL.chains![this.chainId];
       const weth = WETH.chains![this.chainId];
 
       const pair = "0xf82105aA473560CfBF8Cbc6Fd83dB14Eb4028117";
@@ -44,7 +49,7 @@ export class CamelotStrategy extends BaseStrategy<CamelotSupportedChains> {
         args: [
           {
             amountIn: amount,
-            amountOut: BigInt(0),
+            amountOut: BigInt(0), // TODO: amountOut limit
             path: [weth, grail],
             adapters: [adapter],
             recipients: [pair],
@@ -58,7 +63,36 @@ export class CamelotStrategy extends BaseStrategy<CamelotSupportedChains> {
         hash: result,
       });
 
-      return result;
+      const xGRAILBalance = await readContract(wagmiConfig, {
+        address: xGrail,
+        abi: ERC20_ABI,
+        functionName: "balanceOf",
+        args: [user],
+      });
+
+      const approveTx = await writeContract(wagmiConfig, {
+        address: xGrail,
+        abi: XGRAIL_ABI,
+        functionName: "approveUsage",
+        args: [this.dividendsV2, xGRAILBalance],
+      });
+
+      await waitForTransactionReceipt(wagmiConfig, {
+        hash: approveTx,
+      });
+
+      const allocateTx = await writeContract(wagmiConfig, {
+        address: xGrail,
+        abi: XGRAIL_ABI,
+        functionName: "allocate",
+        args: [this.dividendsV2, xGRAILBalance, "0x"],
+      });
+
+      await waitForTransactionReceipt(wagmiConfig, {
+        hash: allocateTx,
+      });
+
+      return allocateTx;
     } else {
       throw new Error("ERC20 is not supported yet");
     }
