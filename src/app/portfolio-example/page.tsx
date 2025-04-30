c626f21a6e1a895529c0e96d420936f9a10018b9
LI-YONG-QI <a2369028@gmail.com>
2025-04-30 17:52:10 +0800

    rm example

--- src/app/portfolio-example/page.tsx
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-// import BalancedRiskPortfolioExample from "@/app/components/RiskPortfolio";
-
-// export default function PortfolioExamplePage() {
-//   return <BalancedRiskPortfolioExample />;
-// }
