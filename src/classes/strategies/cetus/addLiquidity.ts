d6d563c6c1f276212547387dd76e205caf900788
Jarek Lin <s410673074@gmail.com>
2025-05-21 02:16:18 +0800

    feat: cetus add liquidity

---
+++ src/classes/strategies/cetus/addLiquidity.ts
@@ -0,0 +1,88 @@
+import { CETUS_CONTRACTS } from "@/constants/protocols/cetus";
+import { SuiBaseStrategy } from "../base";
+import { Transaction } from "@mysten/sui/transactions";
+import { Address } from "viem";
+import { SUI_TYPE_ARG } from "@mysten/sui/utils";
+import { cetusSDK } from "@/utils/sui";
+import {
+  AddLiquidityFixTokenParams,
+  ClmmPoolUtil,
+  TickMath,
+} from "@cetusprotocol/cetus-sui-clmm-sdk";
+import BN from "bn.js";
+
+export class CetusSwap extends SuiBaseStrategy<typeof CETUS_CONTRACTS> {
+  constructor(chainId: number) {
+    super(chainId, CETUS_CONTRACTS, {
+      protocol: "Cetus",
+      icon: "/crypto-icons/morpho.svg",
+      type: "Trading",
+      description: "Add Liquidity to Cetus",
+    });
+  }
+
+  async buildCalls(
+    amount: bigint,
+    user: Address,
+    asset?: Address,
+  ): Promise<Transaction> {
+    const UsdcSuiPool =
+      "0xb8d7d9e66a60c239e7a60110efcf8de6c705580ed924d0dde141f4a0e2c90105";
+    const pool = await cetusSDK.Pool.getPool(UsdcSuiPool);
+    const lowerTick = TickMath.getPrevInitializableTickIndex(
+      new BN(pool.current_tick_index).toNumber(),
+      new BN(pool.tickSpacing).toNumber(),
+    );
+    const upperTick = TickMath.getNextInitializableTickIndex(
+      new BN(pool.current_tick_index).toNumber(),
+      new BN(pool.tickSpacing).toNumber(),
+    );
+
+    const usdcAmount = new BN(10 ** 4); // 0.01 USDC
+    const fix_amount_a = true;
+    const slippage = 0.01; // 1%
+    const curSqrtPrice = new BN(pool.current_sqrt_price);
+
+    const liquidityInput = ClmmPoolUtil.estLiquidityAndcoinAmountFromOneAmounts(
+      lowerTick,
+      upperTick,
+      usdcAmount,
+      fix_amount_a,
+      true,
+      slippage,
+      curSqrtPrice,
+    );
+
+    const amount_a = fix_amount_a
+      ? usdcAmount.toNumber()
+      : liquidityInput.tokenMaxA.toNumber();
+    const amount_b = fix_amount_a
+      ? liquidityInput.tokenMaxB.toNumber()
+      : usdcAmount.toNumber();
+
+    const addLiquidityPayloadParams: AddLiquidityFixTokenParams = {
+      coinTypeA: pool.coinTypeA,
+      coinTypeB: pool.coinTypeB,
+      pool_id: pool.poolAddress,
+      tick_lower: lowerTick.toString(),
+      tick_upper: upperTick.toString(),
+      fix_amount_a,
+      amount_a,
+      amount_b,
+      slippage,
+      is_open: true,
+      rewarder_coin_types: [],
+      collect_fee: false,
+      pos_id: "",
+    };
+    const tx = await cetusSDK.Position.createAddLiquidityFixTokenPayload(
+      addLiquidityPayloadParams,
+      {
+        slippage: slippage,
+        curSqrtPrice: curSqrtPrice,
+      },
+    );
+
+    return tx;
+  }
+}
--- src/utils/sui.ts
+++ src/utils/sui.ts
@@ -3,7 +3,9 @@ import { normalizeSuiAddress, SUI_TYPE_ARG } from "@mysten/sui/utils";
 import { Transaction } from "@mysten/sui/transactions";
 import { BucketClient } from "bucket-protocol-sdk";
 import { AggregatorClient } from "@cetusprotocol/aggregator-sdk";
+import { initCetusSDK } from "@cetusprotocol/cetus-sui-clmm-sdk";
 
 export const suiClient = new SuiClient({ url: getFullnodeUrl("mainnet") });
 export const bucketClient = new BucketClient();
 export const cetusAggregatorClient = new AggregatorClient({});
+export const cetusSDK = initCetusSDK({ network: "mainnet" });
